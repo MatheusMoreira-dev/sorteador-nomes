f0c45885fa350bc724f5fb87522aa14e0721b49a
unknown <matheus.moreira.marcelino.dev@gmail.com>
2025-10-18 14:04:45 -0300

    gerando um número aleatório por ruído

--- app.js
+++ app.js
@@ -138,20 +138,26 @@ let ganhadores = localStorage.ganhadores
 function addJogadores() {
   // Adiciona novos jogadores e salva no local storage
   jogadores.push(...excludeEqualsValues(splitString(inputNomes.value)));
-  save("jogadores", jogadores);
+  save("jogadores", jogadores.sort());
 
   inputNomes.value = ""; //Limpa Input
   loadJogadores(); // Atualiza lista html dos jogadores
 }
 
+function numeroAleatorio(max) {
+  const array = new Uint32Array(1);
+  window.crypto.getRandomValues(array);
+  return (array[0] / (0xffffffff + 1)) * max;
+}
+
 function sortear() {
   // Sorteia um nome entre os jogadores
-  let randomIndex = parseInt(Math.random() * jogadores.length);
+  let randomIndex = Math.floor(numeroAleatorio(jogadores.length));
   let ganhador = jogadores[randomIndex];
 
   // Retira o ganhador da lista de jogadores
   jogadores.splice(randomIndex, 1);
-  save("jogadores", jogadores);
+  save("jogadores", jogadores.sort());
 
   // Salva o ganhador na lista
   ganhadores.push(ganhador);
